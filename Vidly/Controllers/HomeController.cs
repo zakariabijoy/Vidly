cedf5104c83d402881a2c65eff4dfd0bfc296e10
zakariabijoy <zakaria.bijoy@gmail.com>
2019-11-01 03:04:58 +0600

    Add custom error pages.

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace Vidly.Controllers
 
         public ActionResult About()
         {
+           
             ViewBag.Message = "Your application description page.";
 
             return View();
